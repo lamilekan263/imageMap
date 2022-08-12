84917836ff61088a06b08b4765c75dd3744c8cba
lamilekan263 <lamilekan263@gmail.com>
2022-08-12 11:19:32 +0100

    new push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import { useEffect } from "react";
+import "./App.css"
+
+function App () {
+  let imageWidth = 2048;
+  let  imageHeight = 1364;
+  let  imageAspectRatio = imageWidth / imageHeight;
+   
+
+let hotSpots = [{
+  'title': 'Mouth',
+  'description': 'scream.',
+  'x': -600,
+  'y': -180
+}, {
+  'title': 'Body',
+  'description': 'Look at it.',
+  'x': 108,
+  'y': 20
+}, {
+  'title': 'Antlers',
+  'description': 'They crazy.',
+  'x': 40,
+  'y': -170
+}, {
+  'title': 'This Ear',
+  'description': 'It can hear things.',
+  'x': -265,
+  'y': 145
+}];
+
+
+
+
+  
+  function positionHotSpots() {
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let  windowAspectRatio = windowWidth / windowHeight;
+    const hotSpot = document.querySelectorAll(".hot-spot");
+
+
+    hotSpot.forEach((hotspot, index) => {
+     
+    let xPos = hotSpots[index].x;
+    let yPos = hotSpots[index].y;
+
+
+    if (windowAspectRatio > imageAspectRatio) {
+      yPos = (yPos / imageHeight) * 100;
+      xPos = (xPos / imageWidth) * 100;
+    } else {
+      yPos = ((yPos / (windowAspectRatio / imageAspectRatio)) / imageHeight) * 100;
+      xPos = ((xPos / (windowAspectRatio / imageAspectRatio)) / imageWidth) * 100;
+    }
+
+      hotspot.style.marginTop = `${yPos}%`;
+      hotspot.style.marginLeft = xPos + '%';
+       
+     } )
+       
+     
+  }
+
+  
+   
+  
+  
+
+ useEffect(() => {
+
+         positionHotSpots()
+  
+        window.addEventListener("resize", positionHotSpots);
+        return () => {
+            window.removeEventListener("resize", positionHotSpots)
+        }
+
+       
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div  className='container'>
+      
+      { hotSpots.map(h => <div className="hot-spot"  />)}
+     
+      </div>
+ 
   );
 }
 
